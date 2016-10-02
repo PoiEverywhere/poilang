3141abe190fb3be5fbb62e72ed6a4e38be6d88db
PoiScript <poiscript@gmail.com>
2016-10-02 21:39:19 +0800

    Release 0.0.1

--- poilang.js
+++ poilang.js
@@ -1,45 +1,42 @@
 var fs = require('fs')
   , content = fs.readFileSync('json/all.json')
   , json = JSON.parse(content)
-  , i
   , isChinese = false
   , isEnglish = false
 
 module.exports.random = () => {
-  return console.log(json[Math.floor(Math.random() * json.length) - 1].l[isEnglish ? 1 : (isChinese? 2 : 0)])
+  return console.log(json[Math.floor(Math.random() * json.length) - 1].l[isEnglish? 1 : (isChinese? 2 : 0)])
 }
 
 module.exports.detail = (index) => {
   if (!index && index !== 0) return console.error('No Para!')
   if (typeof index != "number" || index < 0 || index > 999) return console.error('Invalid!')
-  if (index < 100) {
-    i = index
-  } else {
-    switch (index.charAt(0)) {
-      case '1':
-        i = index % 100 + 35
+  if (index >= 100) {
+    switch (Math.floor(index / 100)) {
+      case 1:
+        index = index % 100 + 35
         break
-      case '2':
-        i = index % 100 + 54
+      case 2:
+        index = index % 100 + 54
         break
-      case '3':
-        i = index % 100 + 70
+      case 3:
+        index = index % 100 + 70
         break
       default:
         return console.error('Out Of Range!');
     }
   }
-  if (i > json.length) return console.error('Out Of Range!')
-  console.log('---Poilang---' + '\n' + 'Index:' + json[i].i + '\n' + 'Japnese:' + json[i].j[0] + '\n' +'Source:' + json[i].s)
+  if (index > json.length) return console.error('Out Of Range!')
+  console.log('---Poilang---' + '\n'
+    + 'Index: ' + json[index].i + '\n'
+    + 'Japnese: ' + json[index].l[0] + '\n'
+    + 'English: ' + json[index].l[1] + '\n'
+    + 'Source: ' + json[index].s)
 }
 
 module.exports.switch = (mode) => {
   if (!mode || typeof mode != 'string') return console.error('Invalid')
   switch (mode.toUpperCase()) {
-    // case 'CH':
-    //   isChinese = true
-    //   isEnglish = false
-    //   break
     case 'EN':
       isEnglish = true
       isChinese = false
